a742c538a2d6ce55817e5cafb2c125dfbdf1620a
Chris McNabb <chris.mcnabb@andros.co>
2022-12-12 13:00:24 -0500

    fix: for logic to match var.services struct

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "endpoint_ids" {
   description = "ID of the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.id }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.id }
 }
 
 output "endpoint_arns" {
   description = "ARN of the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.arn }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.arn }
 }
 
 output "endpoint_dns_entries" {
   description = "DNS entries of the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.dns_entry }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.dns_entry }
 }
 
 output "endpoint_network_interface_ids" {
   description = "One or more network interfaces for the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.network_interface_ids }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.network_interface_ids }
 }
 
 output "endpoint_owner_ids" {
   description = "The ID of the AWS account that owns the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.owner_id }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.owner_id }
 }
 
 output "endpoint_states" {
   description = "The state of the VPC endpoints"
-  value       = { for svc in aws_vpc_endpoint.datadog : svc.key => svc.value.state }
+  value       = { for key, svc in aws_vpc_endpoint.datadog : key => svc.state }
 }
